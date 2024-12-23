839617ac1b628886b3c5b66be6643e729221fa8d
Григорий Плаксин <plaksin28@mail.ru>
2024-12-23 18:58:56 +0300

    dsfs

--- backend_foodgram/backend_foodgram/settings.py
+++ backend_foodgram/backend_foodgram/settings.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 
 from api.constants import PER_PAGE
@@ -63,11 +62,11 @@ WSGI_APPLICATION = 'backend_foodgram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'django'),
-        'USER': os.getenv('POSTGRES_USER', 'django'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
-        'PORT': os.getenv('DB_PORT', 5432)
+        'NAME': 'foodgram',
+        'USER': 'foodgram_user',
+        'PASSWORD': 'foodgram_password',
+        'HOST': 'foodgram',
+        'PORT': 5432
     }
 }
 
